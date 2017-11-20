135f0e7dc74a984652d768b62914ff7ae1f55d58
Mark Burnett <mark.m.burnett@gmail.com>
2017-11-20 09:05:29 -0600

    Doc: Add brief getting started with API section
    Change-Id: I5bd0984bbdd65b7ca073d75de611499dab0ad906

--- tools/dev-build.sh
+++ tools/dev-build.sh
@@ -10,4 +10,4 @@ docker build -t quay.io/attcomdev/promenade:latest ${SOURCE_DIR}
 
 export PROMENADE_DEBUG=${PROMENADE_DEBUG:-1}
 
-exec $SCRIPT_DIR/basic-deployment.sh ${@}
+exec $SCRIPT_DIR/simple-deployment.sh ${@}
