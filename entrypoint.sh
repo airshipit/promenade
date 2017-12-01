51df4ce0788ca2c9892d6e10280dfbbd7600129c
Mark Burnett <mark.m.burnett@gmail.com>
2017-12-01 11:06:09 -0600

    Avoid insecure apiserver port for tiller
    This allows us to replace the apiserver process during genesis with the
    chart-managed version that is likely to only listen on a secure port.
    
    * Bundle armada + tiller + insecure apiserver into a static pod
    * Report aramda logs via host filesystem
    
    NOTE: This is using an additional apiserver sidecar rather than a
    `kubectl proxy` sidecar with a serviceaccount, because it's running as a
    static pod.
    
    Change-Id: I39c638020c0ad36db8d3b10c4ecb959a6642ad0e

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ PORT=${PORT:-9000}
 if [ "$1" = 'server' ]; then
     exec uwsgi \
         --http :${PORT} \
+        -z 300 \
         --paste config:/etc/promenade/api-paste.ini \
         --enable-threads -L \
         --workers 4
--- promenade/templates/scripts/genesis.sh
+++ promenade/templates/scripts/genesis.sh
@@ -2,11 +2,16 @@
 
 {% include "up.sh" with context %}
 
+mkdir -p /var/log/armada
+touch /var/log/armada/bootstrap-armada.log
+chmod 777 /var/log/armada/bootstrap-armada.log
+
 set +x
 log
 log === Waiting for Kubernetes API availablity ===
 set -x
 wait_for_kubernetes_api 3600
+
 
 {%- if config['Genesis:labels.dynamic']  is defined %}
 set +x
@@ -21,12 +26,30 @@ log
 log === Deploying bootstrap manifest via Armada ===
 set -x
 
+while [[ ! -e /var/log/armada/bootstrap-armada.log ]]; do
+    sleep 5
+done
+tail -f /var/log/armada/bootstrap-armada.log &
+
+set +x
+end=$(($(date +%s) + 3600))
 while true; do
-    sleep 10
-    if armada apply --debug /etc/genesis/armada/assets/manifest.yaml ; then
+    if [[ -e /etc/kubernetes/manifests/bootstrap-armada.yaml ]]; then
+        now=$(date +%s)
+        if [ $now -gt $end ]; then
+            log Armada static pod manifest still in place after expected duration
+            fail
+        fi
+        sleep 5
+    else
+        log Armada static pod manifest removed
         break
     fi
 done
+set -x
+
+# Terminate background job (tear down exit trap?)
+kill %1
 
 set +x
 log
