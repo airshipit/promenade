1d1b8a7bf962849bc6996d25d25957db68fd0204
Mark Burnett <mark.m.burnett@gmail.com>
2017-12-13 11:40:02 -0600

    Test: extend uwsgi timeout to facilitate testing
    Change-Id: Icd19235e3be8855c2337a752d9e42fcfaada2a33

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 set -ex
 
 PORT=${PORT:-9000}
+UWSGI_TIMEOUT=${UWSGI_TIMEOUT:-300}
 
 if [ "$1" = 'server' ]; then
     exec uwsgi \
         --http :${PORT} \
-        -z 300 \
+        --http-timeout ${UWSGI_TIMEOUT} \
+        -z ${UWSGI_TIMEOUT} \
         --paste config:/etc/promenade/api-paste.ini \
         --enable-threads -L \
         --workers 4
