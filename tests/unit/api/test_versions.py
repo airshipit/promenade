b9dceb2a9a2c23a4569d26efb564b215da9c6eed
Mark Burnett <mark.m.burnett@gmail.com>
2017-12-20 09:29:49 -0600

    Minor testing-related cleanup
    * Move docs dependencies out of test-requirements.txt
    * Eliminate python version specific test (py35)
    * Re-order default envlist to shorten feedback cycle
    * Minor whitespace changes
    * Simplify versions unit test
    * Add /.cache to .gitignore
    * Include tests directory in basic format requirements, but not flake8
    * Fix sample noauth-api-paste.ini config
    
    Change-Id: I440f5c2d988098287f9914449f7103735f832192

--- tests/unit/api/test_versions.py
+++ tests/unit/api/test_versions.py
@@ -1,3 +1,17 @@
+# Copyright 2017 AT&T Intellectual Property.  All other rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import falcon
 from falcon import testing
 import pytest
@@ -10,9 +24,8 @@ from promenade.promenade import promenade
 def client():
     return testing.TestClient(promenade)
 
+
 def test_get_versions(client):
     response = client.simulate_get('/versions')
     assert response.status == falcon.HTTP_200
-    body = response.content.decode('utf-8')
-    assert '"path": "/api/v1.0"' in body
-    assert '"status": "stable"' in body
+    assert response.json == {'v1.0': {'path': '/api/v1.0', 'status': 'stable'}}
