97427904bc69387d365834f73a377a48fa06e813
dt241s@att.com <diwakar.chitoor.thyagaraj@att.com>
2020-08-25 17:22:15 +0000

    Upgrade etcd to 3.4.3
      1) Updated all reference of etcd Imags to 3.4.3
    
    Change-Id: I629af43eb7e9689af3237361cf7a41fc35ed364c

--- tests/unit/api/test_validatedesign.py
+++ tests/unit/api/test_validatedesign.py
@@ -111,7 +111,7 @@ VALID_DOCS = [
                     'controller-manager':
                     'gcr.io/google_containers/hyperkube-amd64:v1.18.6',
                     'etcd':
-                    'quay.io/coreos/etcd:v3.4.2',
+                    'quay.io/coreos/etcd:v3.4.3',
                     'scheduler':
                     'gcr.io/google_containers/hyperkube-amd64:v1.18.6'
                 }
