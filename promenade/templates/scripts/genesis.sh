910b06e4ba37dfe8c60d6299da868c504a0b5ebf
Ruslan Aliev <raliev@mirantis.com>
2024-01-07 19:33:33 -0600

    Add complete support for operator-based bootstrap
     * operator logs is now streaming to pipeline and to pod
     * printing status of armada chart objects
     * adjust armada container cmd parameters to support both
       golang and python based images
    
    Signed-off-by: Ruslan Aliev <raliev@mirantis.com>
    Change-Id: I6d8629a48c1b862db937ddc3cd68792220388b19

--- promenade/templates/scripts/genesis.sh
+++ promenade/templates/scripts/genesis.sh
@@ -7,6 +7,10 @@
 mkdir -p /var/log/armada
 touch /var/log/armada/bootstrap-armada.log
 chmod 777 /var/log/armada/bootstrap-armada.log
+{%- if config['Genesis:enable_operator'] is sameas true %}
+touch /var/log/armada/bootstrap-armada-operator.log
+chmod 777 /var/log/armada/bootstrap-armada-operator.log
+{%- endif %}
 
 {% set metrics_output_dir = config.get_path('Genesis:armada.metrics.output_dir', '/var/log/node-exporter-textfiles') %}
 install -d -m 755 {{ metrics_output_dir }}
@@ -33,16 +37,27 @@ log
 log === Deploying bootstrap manifest via Armada ===
 set -x
 
+{%- if config['Genesis:enable_operator'] is sameas true %}
+while [[ ! -e /var/log/armada/bootstrap-armada.log || ! -e /var/log/armada/bootstrap-armada-operator.log ]]; do
+{%- else %}
 while [[ ! -e /var/log/armada/bootstrap-armada.log ]]; do
+{%- endif %}
     sleep 5
 done
 tail -f /var/log/armada/bootstrap-armada.log &
+{%- if config['Genesis:enable_operator'] is sameas true %}
+tail -f /var/log/armada/bootstrap-armada-operator.log &
+{%- endif %}
 
 set +x
 while true; do
     if [[ -e /etc/kubernetes/manifests/bootstrap-armada.yaml ]]; then
         sleep 30
         kubectl get pods --all-namespaces || echo "Could not get current pod status."
+        {%- if config['Genesis:enable_operator'] is sameas true %}
+        kubectl get armadacharts \
+         -o=custom-columns='NAMESPACE:metadata.namespace,NAME:metadata.name,READY:status.conditions[?(@.type=="Ready")].status,MESSAGE:status.conditions[?(@.type=="Ready")].message' -A || echo "Could not get current armada chart status."
+        {%- endif %}
     else
         log Armada bootstrap manifest deployed
         break
@@ -52,6 +67,9 @@ set -x
 
 # Terminate background job (tear down exit trap?)
 kill %1
+{%- if config['Genesis:enable_operator'] is sameas true %}
+kill %2
+{%- endif %}
 
 set +x
 log
